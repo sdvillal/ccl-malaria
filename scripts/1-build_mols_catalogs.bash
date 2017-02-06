b22828bf5adb601104c198e9f0323ffca16e602c
santi <sdvillal@gmail.com>
2017-02-06 18:05:30 +0100

    move scripts to their own directory

--- scripts/1-build_mols_catalogs.bash
+++ scripts/1-build_mols_catalogs.bash
@@ -1,10 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 pushd `dirname $0` > /dev/null
 myDir=`pwd`
 popd > /dev/null
+
+srcDir="${myDir}/../src"
 
 echo "Welcome to TDT1 Malaria Bootstrapping Script."
 echo "We will download and build catalogs for the competition molecules."
 echo "Go take a nap, (this took one hour in our machine)."
 
-PYTHONPATH="${myDir}/src:${PYTHONPATH}" python2 -u ${myDir}/src/malaria/molscatalog.py init
+PYTHONPATH="${srcDir}:${PYTHONPATH}" python -u ${srcDir}/ccl_malaria/molscatalog.py init
