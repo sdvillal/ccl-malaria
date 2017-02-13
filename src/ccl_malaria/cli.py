0f52f9fe8832cb948b2ad7c1f24d5062b26335c8
santi <sdvillal@gmail.com>
2017-02-13 06:27:23 +0100

    more subcommands for the cli

--- src/ccl_malaria/cli.py
+++ src/ccl_malaria/cli.py
@@ -1,13 +1,18 @@
 # coding=utf-8
 """Centralized access to command line applications."""
 import argh
+
+from ccl_malaria.logregs_analysis import do_logreg_submissions
 from ccl_malaria.molscatalog import init
+from ccl_malaria.results import final_merged_submissions
 
 
 def main():
     parser = argh.ArghParser()
     parser.add_commands([
         init,
+        do_logreg_submissions,
+        final_merged_submissions
     ])
     parser.dispatch()
 
