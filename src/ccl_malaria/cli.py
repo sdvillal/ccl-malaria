79c6de130ccb220f3e7ab3f3d2c98d486aa9d2e0
santi <sdvillal@gmail.com>
2017-02-16 04:12:59 +0100

    one command to rule them all

--- src/ccl_malaria/cli.py
+++ src/ccl_malaria/cli.py
@@ -2,17 +2,50 @@
 """Centralized access to command line applications."""
 import argh
 
-from ccl_malaria.logregs_analysis import do_logreg_submissions
-from ccl_malaria.molscatalog import init
-from ccl_malaria.results import final_merged_submissions
-
 
 def main():
     parser = argh.ArghParser()
+
+    # Initialize the molecules catalog
+    from ccl_malaria.molscatalog import init
     parser.add_commands([
         init,
-        do_logreg_submissions,
-        final_merged_submissions
+    ])
+
+    # Generate features
+    def add_featurizer_commands(parser, namespace='features'):
+        from ccl_malaria.features import cl, ecfps, ecfps_mp, munge_ecfps, rdkfs
+        parser.add_commands([cl,
+                             ecfps,
+                             ecfps_mp,
+                             munge_ecfps,
+                             rdkfs],
+                            namespace=namespace)
+    add_featurizer_commands(parser)
+
+    # Fitting logistic regressions
+    def add_logreg_commands(parser, namespace='logregs'):
+        from ccl_malaria.logregs_fit import cl, fit
+        from ccl_malaria.logregs_analysis import submit
+        parser.add_commands([cl,
+                             fit,
+                             submit],
+                            namespace=namespace)
+    add_logreg_commands(parser)
+
+    # Fitting trees
+    def add_tree_commands(parser, namespace='trees'):
+        from ccl_malaria.trees_fit import fit
+        from ccl_malaria.trees_analysis import submit
+        parser.add_commands([fit,
+                             submit],
+                            namespace=namespace)
+    add_tree_commands(parser)
+
+    # Final dumb blending
+    from ccl_malaria.results import merge_submissions
+    parser.add_commands([
+        merge_submissions
     ])
     parser.dispatch()
 
